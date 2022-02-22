567aa409c30c555b5db887eca2324c8c8a57ec9e
unknown <Qiangwu2000@gmail.com>
2022-02-21 23:06:25 -0600

    :)

---
+++ atom.bat
@@ -0,0 +1,42 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcDeXPmezC7Aa/eTHy++UqVkSRN4+bIDW5pWaL+1d5lDlFQ==
+::fBE1pAF6MU+EWHreyHcjLQlHcDeXPmezC7Aa/eTHy++UqVkSRN4+bIDW5riBJ+9d/l3wFQ==
+::fBE1pAF6MU+EWHreyHcjLQlHcDeXPmezC7Aa/eTHy++UqVkSRN4+bIDW5riBJ+8s+UjleZpjwmJO+A==
+::fBE1pAF6MU+EWHreyHcjLQlHcDeXPmezC7Aa/eTHy++UqVkSRN4+bIDW5rmeNq4Q5UiE
+::fBE1pAF6MU+EWHreyHcjLQlHcDeXPmezC7Aa/eTHy++UqVkSRN4+bIDW5rmeNq4W8kCE
+::fBE1pAF6MU+EWHreyHcjLQlHcDeXPmezC7Aa/eTHy++UqVkSRN4+bIDW5q2BNfQG6ECpcZpj02Jf+A==
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFChBTgiLMW68E7kgzO3o5P6IsnEcQO0yRI7P1bmxKeMc5AvtdplN
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHGQ=
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbAs=
+::ZQ05rAF9IAHYFVzEqQIRPBRZLA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIRPBRZDCmXL2K5RoUQ6fH+/Yo=
+::dhA7uBVwLU+EWGqR9k45JR5SWQjCFWW5SNU=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFChBTgiLMW68E7kgzO3o5P6IsnEcQO0yNorD39Q=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@ECHO OFF
+mpv --idle --load-scripts=no --no-video --no-resume-playback --keep-open-pause=no --quiet            ^
+    --config-dir=configs --msg-time --msg-module --msg-level=cplayer=no,ffmpeg=no --script=Atom.lua
